0683b6f61c0568eb60333f77a65020985570b927
Mason Malone <mason@masonm.org>
2016-09-30 12:42:43 -0400

    Initial commit

---
+++ PP.js
@@ -0,0 +1,106 @@
+var pageDOIs=document.body.innerHTML.match(/\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b/gi);
+pageDOIs = $.unique(pageDOIs);
+if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
+	var storedDOIs=localStorage.getItem("storedDOIs");
+	var dotcom = "devkey=PubMedChrome";
+	var IDsURL = "http://api.pubpeer.com/v2/pubposts?filter=doi&"+dotcom;
+		if(!storedDOIs || storedDOIs.length < 10){
+			var idRequest = new XMLHttpRequest();
+			idRequest.onload = setAllDOIs;
+			idRequest.open("get", IDsURL, false);
+			idRequest.send();
+		}
+	var LastIDdownload=localStorage.getItem("LastIDdownload");
+	if(!LastIDdownload){
+		LastIDdownload = Date.now();
+	}
+		if(((Date.now() - LastIDdownload)/3600000) > 168){
+			var idRequest = new XMLHttpRequest();
+			idRequest.onload = setAllDOIs;
+			idRequest.open("get", IDsURL, false);
+			idRequest.send();
+		}
+
+	function setAllDOIs(){
+		var doiTemp = this.responseText;
+		localStorage.setItem("storedDOIs", doiTemp);
+		storedDOIs = this.responseText;
+		var timeNow = String(Date.now())
+		localStorage.setItem("LastIDdownload", timeNow);
+	}
+	function getDetails(){
+		var articleRequest = new XMLHttpRequest();
+		articleRequest.onload = getPpJson;
+		articleRequest.open("get", address, false);
+		articleRequest.send();
+	}
+	function getPpJson(){
+		json = $.parseJSON(this.responseText);
+	}
+
+	var allDOIs = $.parseJSON(storedDOIs);
+
+
+	
+
+	var matchedPageDOIs = new Array();
+	var doisPpLink = "";
+	var re = null;
+	for(i=0; i<pageDOIs.length; i++){
+		if(allDOIs.dois.indexOf(pageDOIs[i]) != -1){
+			matchedPageDOIs.push(pageDOIs[i]);
+			doisPpLink += pageDOIs[i] + ";";
+		}
+	}
+
+	if(matchedPageDOIs.length > 0){
+		var address = "http://api.pubpeer.com/v2/publications/" + doisPpLink + "?" + dotcom;
+		getDetails();
+	}
+
+
+	var hrefText
+	for(i=0; i<matchedPageDOIs.length; i++){
+		var total_comments = json.feedbacks[i].total_comments;
+		if(total_comments == 1){
+			hrefText = "1 comment on PubPeer";
+		}
+		if(total_comments > 1){
+			hrefText = total_comments + " comments on PubPeer";
+		}
+		var linkToComments = json.feedbacks[i].url + "?utm_source=Chrome&utm_medium=ChromeExtension&utm_campaign=Extensions";
+		
+		var tagElements = ":contains("+matchedPageDOIs[i]+")";
+		var unsortedDoiElements = $(tagElements);
+		var aDoiElement = [];
+		if(unsortedDoiElements.length>0){
+			for(m=0; m<unsortedDoiElements.length; m++){
+				var allParents = $(unsortedDoiElements[m]).parents().length
+				aDoiElement.push({element:unsortedDoiElements[m], rents:allParents});
+			}
+			aDoiElement.sort(function(a,b){
+				return b.rents - a.rents;
+			});
+		}
+		var elementsWithDois = aDoiElement.length
+			for(k=0; k<elementsWithDois; k++){	//try each element that contains a matched DOI
+				var pp_commClass = document.getElementsByClassName('pp_comm');
+				var elementsAdded = pp_commClass.length;
+				var anyAlreadyAdded = 0;
+				for(l=0; l == 0 || l<elementsAdded; l++){	//check if an existing tag is nested below the element
+					var alreadyAdded = $.contains(aDoiElement[k].element, pp_commClass[l]);
+					if(alreadyAdded == true){
+						anyAlreadyAdded += 1
+					}
+					
+				}
+			if(anyAlreadyAdded < 1){
+				$(aDoiElement[k].element).append(
+					$("<p>", { class: "pp_comm" })
+						.append($("<a>", { href: linkToComments, style: "color: rgb(255,255,255); text-decoration: none; font-weight: bold; margin-left: 1em", text: hrefText}))
+						.css("background", "#ff9e29")
+				);
+			}
+		}
+	}
+}
