8662845275b185dd4f56657a4e4aed7dd808410f
Mason Malone <mason@masonm.org>
2016-09-30 22:40:50 -0400

    Minor cleanup for PP.js

--- PP.js
+++ PP.js
@@ -4,6 +4,7 @@ if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
 	var storedDOIs=localStorage.getItem("storedDOIs");
 	var dotcom = "devkey=PubMedChrome";
 	var IDsURL = "http://api.pubpeer.com/v2/pubposts?filter=doi&"+dotcom;
+
 	if(!storedDOIs || storedDOIs.length < 10){
 		var idRequest = new XMLHttpRequest();
 		idRequest.onload = setAllDOIs;
@@ -22,11 +23,9 @@ if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
 	}
 
 	function setAllDOIs(){
-		var doiTemp = this.responseText;
-		localStorage.setItem("storedDOIs", doiTemp);
+		localStorage.setItem("storedDOIs", this.responseText);
 		storedDOIs = this.responseText;
-		var timeNow = String(Date.now())
-		localStorage.setItem("LastIDdownload", timeNow);
+		localStorage.setItem("LastIDdownload", String(Date.now()));
 	}
 	function getDetails(){
 		var articleRequest = new XMLHttpRequest();
@@ -39,13 +38,10 @@ if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
 	}
 
 	var allDOIs = $.parseJSON(storedDOIs);
-
-
 	
 
 	var matchedPageDOIs = new Array();
 	var doisPpLink = "";
-	var re = null;
 	for(i=0; i<pageDOIs.length; i++){
 		if(allDOIs.dois.indexOf(pageDOIs[i]) != -1){
 			matchedPageDOIs.push(pageDOIs[i]);
@@ -59,13 +55,11 @@ if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
 	}
 
 
-	var hrefText
 	for(i=0; i<matchedPageDOIs.length; i++){
 		var total_comments = json.feedbacks[i].total_comments;
 		if(total_comments == 1){
 			hrefText = "1 comment on PubPeer";
-		}
-		if(total_comments > 1){
+		} else if(total_comments > 1){
 			hrefText = total_comments + " comments on PubPeer";
 		}
 		var linkToComments = json.feedbacks[i].url + "?utm_source=Chrome&utm_medium=ChromeExtension&utm_campaign=Extensions";
@@ -86,15 +80,14 @@ if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
 		for(k=0; k<elementsWithDois; k++){	//try each element that contains a matched DOI
 			var pp_commClass = document.getElementsByClassName('pp_comm');
 			var elementsAdded = pp_commClass.length;
-			var anyAlreadyAdded = 0;
+			var anyAlreadyAdded = false;
 			for(l=0; l == 0 || l<elementsAdded; l++){	//check if an existing tag is nested below the element
 				var alreadyAdded = $.contains(aDoiElement[k].element, pp_commClass[l]);
 				if(alreadyAdded == true){
-					anyAlreadyAdded += 1
+					anyAlreadyAdded = true;
 				}
-				
 			}
-			if(anyAlreadyAdded < 1){
+			if(!anyAlreadyAdded){
 				$(aDoiElement[k].element).append(
 					$("<p>", { class: "pp_comm" })
 						.append($("<a>", { href: linkToComments, style: "color: rgb(255,255,255); text-decoration: none; font-weight: bold; margin-left: 1em", text: hrefText}))
