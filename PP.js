965f512498cbe2a10fcd289ae90a73256baf4d67
Mason Malone <mason@masonm.org>
2016-10-01 14:52:34 -0400

    Fix issues with synchronous XHRs
    This uses Promises to asynchronously load content from Pubpeer. This means it doesn't block the page
    from loading when api.pubpeer.com is down and won't break when Chrome and Firefox remove support for
    synchronous XHR: https://developer.chrome.com/apps/app_deprecated

--- PP.js
+++ PP.js
@@ -1,99 +1,96 @@
 var pageDOIs=document.body.innerHTML.match(/\b(10[.][0-9]{4,}(?:[.][0-9]+)*\/(?:(?!["&\'<>])\S)+)\b/gi);
 pageDOIs = $.unique(pageDOIs);
 if(window.location.hostname != "pubpeer.com" && pageDOIs.length > 0){
-	var storedDOIs=localStorage.getItem("storedDOIs");
-	var dotcom = "devkey=PubMedChrome";
-	var IDsURL = "http://api.pubpeer.com/v2/pubposts?filter=doi&"+dotcom;
+	let dotcom = "devkey=PubMedChrome";
 
-	if(!storedDOIs || storedDOIs.length < 10){
-		var idRequest = new XMLHttpRequest();
-		idRequest.onload = setAllDOIs;
-		idRequest.open("get", IDsURL, false);
-		idRequest.send();
-	}
-	var LastIDdownload=localStorage.getItem("LastIDdownload");
-	if(!LastIDdownload){
-		LastIDdownload = Date.now();
-	}
-	if(((Date.now() - LastIDdownload)/3600000) > 168){
-		var idRequest = new XMLHttpRequest();
-		idRequest.onload = setAllDOIs;
-		idRequest.open("get", IDsURL, false);
-		idRequest.send();
-	}
+	new Promise(function(resolve, reject) {
+		let storedDOIs = localStorage.getItem("storedDOIs");
+		let LastIDdownload = localStorage.getItem("LastIDdownload");
 
-	function setAllDOIs(){
-		localStorage.setItem("storedDOIs", this.responseText);
-		storedDOIs = this.responseText;
-		localStorage.setItem("LastIDdownload", String(Date.now()));
-	}
-	function getDetails(){
-		var articleRequest = new XMLHttpRequest();
-		articleRequest.onload = getPpJson;
-		articleRequest.open("get", address, false);
-		articleRequest.send();
-	}
-	function getPpJson(){
-		json = $.parseJSON(this.responseText);
-	}
-
-	var allDOIs = $.parseJSON(storedDOIs);
-	
-
-	var matchedPageDOIs = new Array();
-	var doisPpLink = "";
-	for(i=0; i<pageDOIs.length; i++){
-		if(allDOIs.dois.indexOf(pageDOIs[i]) != -1){
-			matchedPageDOIs.push(pageDOIs[i]);
-			doisPpLink += pageDOIs[i] + ";";
+		if (
+			!storedDOIs
+			|| storedDOIs.length < 10
+			|| !LastIDdownload
+			|| ((Date.now() - LastIDdownload) > (168 * 60 * 60 * 1000))
+		) {
+			let IDsURL = "http://api.pubpeer.com/v2/pubposts?filter=doi&"+dotcom;
+			let idRequest = new XMLHttpRequest();
+			idRequest.onload = function () {
+				localStorage.setItem("storedDOIs", this.responseText);
+				localStorage.setItem("LastIDdownload", String(Date.now()));
+				resolve(this.responseText);
+			};
+			idRequest.open("get", IDsURL);
+			idRequest.send();
+		} else {
+			resolve(storedDOIs);
 		}
-	}
-
-	if(matchedPageDOIs.length > 0){
-		var address = "http://api.pubpeer.com/v2/publications/" + doisPpLink + "?" + dotcom;
-		getDetails();
-	}
-
-
-	for(i=0; i<matchedPageDOIs.length; i++){
-		var total_comments = json.feedbacks[i].total_comments;
-		if(total_comments == 1){
-			hrefText = "1 comment on PubPeer";
-		} else if(total_comments > 1){
-			hrefText = total_comments + " comments on PubPeer";
-		}
-		var linkToComments = json.feedbacks[i].url + "?utm_source=Chrome&utm_medium=ChromeExtension&utm_campaign=Extensions";
-		
-		var tagElements = ":contains("+matchedPageDOIs[i]+")";
-		var unsortedDoiElements = $(tagElements);
-		var aDoiElement = [];
-		if(unsortedDoiElements.length>0){
-			for(m=0; m<unsortedDoiElements.length; m++){
-				var allParents = $(unsortedDoiElements[m]).parents().length
-				aDoiElement.push({element:unsortedDoiElements[m], rents:allParents});
+	}).then(function(storedDOIs) {
+		let allDOIs = $.parseJSON(storedDOIs);
+		let matchedPageDOIs = new Array();
+		let address = "http://api.pubpeer.com/v2/publications/"
+		for(let i=0; i<pageDOIs.length; i++){
+			if(allDOIs.dois.indexOf(pageDOIs[i]) != -1){
+				matchedPageDOIs.push(pageDOIs[i]);
+				address += pageDOIs[i] + ";";
 			}
-			aDoiElement.sort(function(a,b){
-				return b.rents - a.rents;
-			});
 		}
-		var elementsWithDois = aDoiElement.length
-		for(k=0; k<elementsWithDois; k++){	//try each element that contains a matched DOI
-			var pp_commClass = document.getElementsByClassName('pp_comm');
-			var elementsAdded = pp_commClass.length;
-			var anyAlreadyAdded = false;
-			for(l=0; l == 0 || l<elementsAdded; l++){	//check if an existing tag is nested below the element
-				var alreadyAdded = $.contains(aDoiElement[k].element, pp_commClass[l]);
-				if(alreadyAdded == true){
-					anyAlreadyAdded = true;
+
+		return new Promise(function(resolve, reject) {
+			if (matchedPageDOIs.length == 0) {
+				resolve();
+			}
+			let articleRequest = new XMLHttpRequest();
+			articleRequest.onload = function() {
+				resolve([this.responseText, matchedPageDOIs]);
+			};
+			articleRequest.open("get", address + "?" + dotcom);
+			articleRequest.send();
+		});
+	}).then(function(responseAndDois) {
+		if (!responseAndDois) { return; }
+		let json = $.parseJSON(responseAndDois[0]);
+		let matchedPageDOIs = responseAndDois[1];
+		for(let i=0; i<matchedPageDOIs.length; i++){
+			let total_comments = json.feedbacks[i].total_comments;
+			let hrefText;
+			if(total_comments == 1){
+				hrefText = "1 comment on PubPeer";
+			} else if(total_comments > 1){
+				hrefText = total_comments + " comments on PubPeer";
+			}
+			let linkToComments = json.feedbacks[i].url + "?utm_source=Chrome&utm_medium=ChromeExtension&utm_campaign=Extensions";
+			let tagElements = ":contains("+matchedPageDOIs[i]+")";
+			let unsortedDoiElements = $(tagElements);
+			let aDoiElement = [];
+			if(unsortedDoiElements.length>0){
+				for(let m=0; m<unsortedDoiElements.length; m++){
+					var allParents = $(unsortedDoiElements[m]).parents().length
+					aDoiElement.push({element:unsortedDoiElements[m], rents:allParents});
+				}
+				aDoiElement.sort(function(a,b){
+					return b.rents - a.rents;
+				});
+			}
+			let elementsWithDois = aDoiElement.length
+			for(let k=0; k<elementsWithDois; k++){	//try each element that contains a matched DOI
+				let pp_commClass = document.getElementsByClassName('pp_comm');
+				let elementsAdded = pp_commClass.length;
+				let anyAlreadyAdded = false;
+				for(let l=0; l == 0 || l<elementsAdded; l++){	//check if an existing tag is nested below the element
+					var alreadyAdded = $.contains(aDoiElement[k].element, pp_commClass[l]);
+					if(alreadyAdded == true){
+						anyAlreadyAdded = true;
+					}
+				}
+				if(!anyAlreadyAdded){
+					$(aDoiElement[k].element).append(
+						$("<p>", { class: "pp_comm" })
+							.append($("<a>", { href: linkToComments, style: "color: rgb(255,255,255); text-decoration: none; font-weight: bold; margin-left: 1em", text: hrefText}))
+							.css("background", "#ff9e29")
+					);
 				}
 			}
-			if(!anyAlreadyAdded){
-				$(aDoiElement[k].element).append(
-					$("<p>", { class: "pp_comm" })
-						.append($("<a>", { href: linkToComments, style: "color: rgb(255,255,255); text-decoration: none; font-weight: bold; margin-left: 1em", text: hrefText}))
-						.css("background", "#ff9e29")
-				);
-			}
 		}
-	}
+	});
 }
